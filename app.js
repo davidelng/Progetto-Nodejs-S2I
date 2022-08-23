8de08de6a2db8f14c08c81f6b54c13eec7d6d9c9
Davide Longo <davide.longo@dedagroup.it>
2022-08-23 16:29:12 +0200

    env example, app config

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 require("dotenv").config();
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Now listening on port " + process.env.PORT)
-);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "L'app funziona correttamente" });
+});
+
+app.use((req, res, next) => {
+  res.status(404).send({ message: "La pagina è inesistente" });
+});
+
+app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
