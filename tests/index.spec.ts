143985939ce3e70cde31a462967d1fae059fbea9
Davide Longo <davide.longo@dedagroup.it>
2022-09-12 12:02:58 +0200

    index tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -6,13 +6,17 @@ describe("Node REST API app", () => {
 
     const app = express();
 
+    app.get("/", (req, res) => {
+        res.status(200).send("OK");
+    });
+
+    app.use((req, res) => {
+        res.status(404).send("Not found");
+    });
+
     before((done) => {
         app.listen(3000, () => {
           done();
-        });
-
-        app.get("/", (req, res) => {
-            res.status(200).send("OK");
         });
     });
 
@@ -22,6 +26,10 @@ describe("Node REST API app", () => {
 
     it("Should create a server without errors", (done) => {
         request(app).get("/").expect(200, done);
+    });
+
+    it("Should handle undefined routes", (done) => {
+        request(app).get("/asjdhk").expect(404, done);
     });
 
 });
